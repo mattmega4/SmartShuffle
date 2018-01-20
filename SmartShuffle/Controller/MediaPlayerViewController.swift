b7c851ce0b71ff9858396f682f8bc94b80542fa7
mattmega4 <mattmega4@gmail.com>
2018-01-20 11:41:06 -0800

    Added plist rule

--- SmartShuffle/Controller/MediaPlayerViewController.swift
+++ SmartShuffle/Controller/MediaPlayerViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class MediaPlayerViewController: UIViewController {
   
   @IBOutlet weak var albumArtImageView: UIImageView!
@@ -44,6 +46,8 @@ class MediaPlayerViewController: UIViewController {
   }
   
 
+  
+  
   
   
   
